5f387da4a220ba65b882d5882c4e35d90534290a
linusmbae <linusmutugi5178@gmail.com>
2020-05-22 21:41:50 +0300

    create ProductListsAdapter

---
+++ app/src/main/java/com/linus/cynix/ProductListsAdapter.java
@@ -0,0 +1,28 @@
+package com.linus.cynix;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class ProductListsAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
