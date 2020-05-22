8e851906538cb9eec7f52ee3c546b2af125a9611
linusmbae <linusmutugi5178@gmail.com>
2020-05-22 17:53:44 +0300

    test for button click passed

--- app/src/androidTest/java/com/linus/cynix/MainActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/linus/cynix/MainActivityInstrumentedTest.java
@@ -1,11 +1,17 @@
 package com.linus.cynix;
 
+import android.widget.Toast;
+
 import androidx.test.rule.ActivityTestRule;
 
+import static androidx.test.espresso.Espresso.onView;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.action.ViewActions.click;
 import androidx.test.filters.LargeTest;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
@@ -13,4 +19,9 @@ import org.junit.runner.RunWith;
 public class MainActivityInstrumentedTest {
 @Rule
     public ActivityTestRule<MainActivity>activityTestRule=new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void TestButtonClick()throws Exception {
+        onView(withId(R.id.SignUpButton)).perform(click());
+    }
 }
