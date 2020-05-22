693cb60c61f5adcf92404a5d2a10fcc1365fafda
linusmbae <linusmutugi5178@gmail.com>
2020-05-22 23:33:07 +0300

    datagridview display complete

--- app/src/main/java/com/linus/cynix/ProductListsAdapter.java
+++ app/src/main/java/com/linus/cynix/ProductListsAdapter.java
@@ -5,20 +5,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ProductListsAdapter extends BaseAdapter {
 private String[] mMenWear;
 private Context mContext;
+private int[] mImages;
 
-    public ProductListsAdapter(String[] menWear,Context context) {
+    public ProductListsAdapter(String[] menWear,Context context,int[] images) {
         this.mMenWear = menWear;
         this.mContext= context;
+        this.mImages=images;
     }
 
     @Override
     public int getCount() {
-        return mMenWear.length;
+        return mImages.length;
     }
 
     @Override
@@ -38,10 +41,13 @@ private Context mContext;
         View gridView;
         if (convertView==null)
         {
-            gridView=layoutInflater.inflate(R.layout.products_grid_view1,null);
+            gridView=layoutInflater.inflate(R.layout.products_grid_view_row,null);
             TextView letterView = (TextView) gridView
-                    .findViewById(R.id.grid_item_picture);
+                    .findViewById(R.id.grid_item_label1);
+            ImageView imageView=(ImageView)gridView
+            .findViewById(R.id.imageView);
             letterView.setText(mMenWear[position]);
+            imageView.setImageResource(mImages[position]);
         }else {
             gridView = (View) convertView;
         }
--- app/src/main/java/com/linus/cynix/ProductsListActivity.java
+++ app/src/main/java/com/linus/cynix/ProductsListActivity.java
@@ -7,13 +7,14 @@ import android.widget.GridView;
 
 public class ProductsListActivity extends AppCompatActivity {
     GridView gridView;
-    private String[] menWear=new String[]{""};
+    private String [] menWear=new String[]{"Men wear 1", "Men wear 2","Men wear 3", "Men wear 4","Men wear 5", "Men wear 6"};
+    private int[]images=  new int[]{R.drawable.tyler,R.drawable.tamarcus,R.drawable.fabio,R.drawable.bruno,R.drawable.terricks,R.drawable.taylor};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_products_list);
+        setContentView(R.layout.activity_products_list_column);
         gridView = (GridView) findViewById(R.id.baseGridView);
-        gridView.setAdapter(new ProductListsAdapter(this, menWear));
+        gridView.setAdapter(new ProductListsAdapter( menWear,this,images));
     }
 }
