8f695b92c3078a48b89c104fd9d7e420db56fbd6
linusmbae <linusmutugi5178@gmail.com>
2020-05-22 21:35:53 +0300

    grid design complete

--- app/src/main/java/com/linus/cynix/ProductsListActivity.java
+++ app/src/main/java/com/linus/cynix/ProductsListActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class ProductsListActivity extends AppCompatActivity {
 
+    private String[] menWear=new String[]{""};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
