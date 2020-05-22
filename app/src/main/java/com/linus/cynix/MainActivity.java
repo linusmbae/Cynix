d20bde0c540848c6447f45833679fcda4583abed
linusmbae <linusmutugi5178@gmail.com>
2020-05-22 17:36:38 +0300

    Button Effective

--- app/src/main/java/com/linus/cynix/MainActivity.java
+++ app/src/main/java/com/linus/cynix/MainActivity.java
@@ -3,12 +3,30 @@ package com.linus.cynix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+@BindView(R.id.SignUpButton)Button mSignUpButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        mSignUpButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if (v==mSignUpButton)
+        {
+            Toast.makeText(MainActivity.this,"Clicked",Toast.LENGTH_LONG).show();
+        }
     }
 }
