7864c18ddd382947038e4f0dc2fb0e31247d1429
linusmbae <linusmutugi5178@gmail.com>
2020-05-22 18:21:50 +0300

    SignUpDialogPage accessible on button click

--- app/src/main/java/com/linus/cynix/MainActivity.java
+++ app/src/main/java/com/linus/cynix/MainActivity.java
@@ -1,7 +1,9 @@
 package com.linus.cynix;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,7 +28,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     {
         if (v==mSignUpButton)
         {
-            Toast.makeText(MainActivity.this,"Clicked",Toast.LENGTH_LONG).show();
+
+            FragmentManager fm=getSupportFragmentManager();
+            SignUpDialogPage signUpDialogPage= new SignUpDialogPage();
+            signUpDialogPage.show(fm,"Sample Fragment");
+//            Toast.makeText(MainActivity.this,"Clicked",Toast.LENGTH_LONG).show();
         }
     }
 }
--- app/src/main/java/com/linus/cynix/SignUpDialogPage.java
+++ app/src/main/java/com/linus/cynix/SignUpDialogPage.java
@@ -1,4 +1,20 @@
 package com.linus.cynix;
 
-public class SignUpDialogPage {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+public class SignUpDialogPage extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
+        getDialog().setTitle("Simple Dialog");
+        return rootView;
+    }
 }
