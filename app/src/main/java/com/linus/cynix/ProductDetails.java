e3ae9335ae0affb0677cf5b25e3b50bbb11288c3
linusmbae <linusmutugi5178@gmail.com>
2020-05-23 10:49:56 +0300

    activity product details complete

--- app/src/main/java/com/linus/cynix/ProductDetails.java
+++ app/src/main/java/com/linus/cynix/ProductDetails.java
@@ -2,13 +2,27 @@ package com.linus.cynix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ProductDetails extends AppCompatActivity {
 
+   @BindView(R.id.dataText)TextView mDataText;
+   @BindView(R.id.myImage)ImageView mMyImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+
+        ButterKnife.bind(this);
+
+        Intent intent=getIntent();
+        mDataText.setText(intent.getStringExtra("wearName"));
+        mMyImage.setImageResource(intent.getIntExtra("product",0));
     }
 }
