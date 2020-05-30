760637ec07c8dea201bdc01d48c4011a80396b16
linusmbae <linusmutugi5178@gmail.com>
2020-05-31 00:34:47 +0300

    CynixBusinessesSearchResponse complete

--- app/src/main/java/com/linus/cynix/CynixBusinessesSearchResponse.java
+++ app/src/main/java/com/linus/cynix/CynixBusinessesSearchResponse.java
@@ -1,4 +1,36 @@
 package com.linus.cynix;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class CynixBusinessesSearchResponse {
+    @SerializedName("shops")
+    @Expose
+    private List<Shops> shops = null;
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public CynixBusinessesSearchResponse() {
+    }
+
+    /**
+     *
+     * @param shops
+     */
+
+    public CynixBusinessesSearchResponse(List<Shops> shops) {
+        this.shops = shops;
+    }
+
+    public List<Shops> getShops() {
+        return shops;
+    }
+
+    public void setShops(List<Shops> shops) {
+        this.shops = shops;
+    }
 }
