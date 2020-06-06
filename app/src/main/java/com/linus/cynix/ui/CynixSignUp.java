c2a0a16b08d097a50db012038066842453c37861
linusmbae <linusmutugi5178@gmail.com>
2020-06-06 14:03:17 +0300

    sign up page design complete

---
+++ app/src/main/java/com/linus/cynix/ui/CynixSignUp.java
@@ -0,0 +1,16 @@
+package com.linus.cynix.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.linus.cynix.R;
+
+public class CynixSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cynix_sign_up);
+    }
+}
