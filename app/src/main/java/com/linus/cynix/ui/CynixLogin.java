66d76aadf472c9db4d1eac3708d6ce9bb90b6888
linusmbae <linusmutugi5178@gmail.com>
2020-06-06 15:15:30 +0300

    login design modification effective

--- app/src/main/java/com/linus/cynix/ui/CynixLogin.java
+++ app/src/main/java/com/linus/cynix/ui/CynixLogin.java
@@ -2,15 +2,33 @@ package com.linus.cynix.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.linus.cynix.R;
 
-public class CynixLogin extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class CynixLogin extends AppCompatActivity implements View.OnClickListener {
+@BindView(R.id.txtSignUp)
+    TextView mSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cynix_login);
+
+        ButterKnife.bind(this);
+        mSignup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+if (v==mSignup){
+    Intent intent=new Intent(CynixLogin.this,CynixSignUp.class);
+    startActivity(intent);
+}
     }
 }
--- app/src/main/java/com/linus/cynix/ui/CynixSignUp.java
+++ app/src/main/java/com/linus/cynix/ui/CynixSignUp.java
@@ -2,15 +2,33 @@ package com.linus.cynix.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.linus.cynix.R;
 
-public class CynixSignUp extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class CynixSignUp extends AppCompatActivity implements View.OnClickListener{
+@BindView(R.id.txtLogin)
+    TextView mLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cynix_sign_up);
+        ButterKnife.bind(this);
+        mLogin.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+if (v==mLogin){
+    Intent intent=new Intent(CynixSignUp.this,CynixLogin.class);
+    startActivity(intent);
+}
     }
 }
